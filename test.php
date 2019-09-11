813ce8dbc258ea4417beffeb7626905f0d28e8ef
Bow Vernon <bowvernon110391@gmail.com>
2019-09-11 20:29:03 +0700

    Add reminder on test.php

--- test.php
+++ test.php
@@ -3,4 +3,8 @@ $name  = 'shit.ctl';
 
 $s = json_decode(file_get_contents('settings.json'));
 print_r($s);
+
+// gonna add csv tester below, to test csv import
+// as of now, csv import is rather fragile, not all case is covered
+// because we depends on php's csv reader
 ?>
